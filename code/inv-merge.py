2c00cb8c03e344e7e878a5d9ac0b70b2e0b55b76
Artorias1469 <pupas5676@gmail.com>
2023-12-01 19:29:27 +0300

    all

---
+++ code/inv-merge.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+
+def count_inversions(arr):
+    if len(arr) <= 1:
+        return arr, 0
+
+    mid = len(arr) // 2
+    left, inv_left = count_inversions(arr[:mid])
+    right, inv_right = count_inversions(arr[mid:])
+
+    merged, inv_merge = merge_and_count(left, right)
+
+    return merged, inv_left + inv_right + inv_merge
+
+def merge_and_count(left, right):
+    merged = []
+    inversions = 0
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            merged.append(left[i])
+            i += 1
+        else:
+            merged.append(right[j])
+            inversions += len(left) - i
+            j += 1
+
+    merged.extend(left[i:])
+    merged.extend(right[j:])
+
+    return merged, inversions
+
+if __name__ == "__main__":
+    arr = [1, 3, 5, 2, 4, 6]
+    sorted_arr, inversions = count_inversions(arr)
+
+    print("Исходный массив:", arr)
+    print("Количество инверсий:", inversions)
