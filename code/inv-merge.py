f27d81679a0f3163cb0aed676b177fa41eb930d1
Artorias1469 <pupas5676@gmail.com>
2023-12-04 09:03:08 +0300

    vbn

--- code/inv-merge.py
+++ code/inv-merge.py
@@ -9,36 +9,18 @@ def count_inversions(arr):
     left, inv_left = count_inversions(arr[:mid])
     right, inv_right = count_inversions(arr[mid:])
 
-    merged, inv_merge = merge_and_count(left, right)
+    merged, inv_merge = merge_and_count(left, right, 0, 0, [])
 
     return merged, inv_left + inv_right + inv_merge
 
-def merge_and_count(left, right):
-    merged = []
-    inversions = 0
-    i = j = 0
-
-    def merge():
-        nonlocal i, j, inversions
+def merge_and_count(left, right, i, j, merged):
+    if i < len(left) and j < len(right):
         if left[i] <= right[j]:
-            merged.append(left[i])
-            i += 1
+            return merge_and_count(left, right, i + 1, j, merged + [left[i]])
         else:
-            merged.append(right[j])
-            inversions += len(left) - i
-            j += 1
-
-    def merge_remaining():
-        nonlocal i, j
-        merged.extend(left[i:])
-        merged.extend(right[j:])
-
-    while i < len(left) and j < len(right):
-        merge()
-
-    merge_remaining()
-
-    return merged, inversions
+            return merge_and_count(left, right, i, j + 1, merged + [right[j]]) + (len(left) - i,)
+    else:
+        return merged + left[i:] + right[j:], 0
 
 if __name__ == "__main__":
     arr = [1, 3, 5, 2, 4, 6]
